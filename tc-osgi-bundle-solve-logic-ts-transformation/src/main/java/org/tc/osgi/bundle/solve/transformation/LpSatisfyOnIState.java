4c95f0c42efe342e179214dbeca2679db71f0b71
Thomas Collonville <collonville_tom@yahoo.fr>
2018-01-27 00:29:21 +0100

    correction erreur compilation

--- tc-osgi-bundle-solve-logic-ts-transformation/src/main/java/org/tc/osgi/bundle/solve/transformation/LpSatisfyOnIState.java
+++ tc-osgi-bundle-solve-logic-ts-transformation/src/main/java/org/tc/osgi/bundle/solve/transformation/LpSatisfyOnIState.java
@@ -3,11 +3,12 @@ package org.tc.osgi.bundle.solve.transformation;
 import org.tc.osgi.bundle.logic.lp.metamodel.Pa;
 import org.tc.osgi.bundle.logic.m3.IPa;
 import org.tc.osgi.bundle.solve.m3.AbstractSatisfy;
+import org.tc.osgi.bundle.solve.transformation.module.service.CollectionUtilsServiceProxy;
 import org.tc.osgi.bundle.ts.m3.core.IProperty;
 import org.tc.osgi.bundle.ts.m3.core.IState;
 import org.tc.osgi.bundle.ts.m3.core.ITs;
-import org.tc.osgi.bundle.utils.collection.Collections;
-import org.tc.osgi.bundle.utils.collection.IPredicate;
+import org.tc.osgi.bundle.utils.interf.collection.IPredicate;
+
 
 /**
  * LpSatisfyOnIState.java.
@@ -38,7 +39,7 @@ public class LpSatisfyOnIState extends AbstractSatisfy {
      */
     private void analyseState(final IState state) {
         final String name = state.getName();
-        final IState s = Collections.getInstance().extract(getTransitionStructure().getStates(), new IPredicate<IState>() {
+        final IState s = CollectionUtilsServiceProxy.getInstance().extract(getTransitionStructure().getStates(), new IPredicate<IState>() {
             @Override
             public boolean evaluate(final IState e1) {
                 if (e1.getName().equals(name)) {
@@ -79,7 +80,7 @@ public class LpSatisfyOnIState extends AbstractSatisfy {
      */
     public IPa getPa(final String pa) {
         final String name = pa;
-        return Collections.getInstance().extract(getPropertySet(), new IPredicate<IPa>() {
+        return CollectionUtilsServiceProxy.getInstance().extract(getPropertySet(), new IPredicate<IPa>() {
 
             @Override
             public boolean evaluate(final IPa e1) {
